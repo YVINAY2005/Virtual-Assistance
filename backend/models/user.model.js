8ca66ae84af19e5c7e53f31a99b86fe868a67fc7
YVINAY2005 <yvinat541@gmail.com>
2025-11-29 18:08:10 +0530

    updated files

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -32,5 +32,6 @@ const userSchema=new mongoose.Schema({
 
 },{timestamps:true})
 
+
 const User=mongoose.model("User",userSchema)
 export default User
--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,8 @@ dotenv.config();
 const app= express();
 
 app.use(cors({
-    origin:process.env.VITE_FRONTEND_URL || "http://localhost:5173",
-
-    credentials:true
+    origin: [process.env.VITE_FRONTEND_URL || "http://localhost:5173", "http://localhost:5173"],
+    credentials: true
 }))
 
 const port=process.env.PORT ||5000
