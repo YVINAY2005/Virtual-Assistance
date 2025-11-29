11f11f0efbc1c8d22feca4b1a5f04af5d738582a
YVINAY2005 <yvinat541@gmail.com>
2025-11-29 19:20:35 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app= express();
 
 app.use(cors({
-    origin: [process.env.VITE_FRONTEND_URL || "http://localhost:5173", "http://localhost:5173"],
+    origin: [process.env.VITE_FRONTEND_URL || "http://localhost:5173", "http://localhost:5173", "https://virtual-assistance-frontend.onrender.com"],
     credentials: true
 }))
 
